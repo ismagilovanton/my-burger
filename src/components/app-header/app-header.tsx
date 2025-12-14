aef76dafd8ae844a7520d3f9eb08474725482ec3
ismagilov_anton <oldvertu@gmail.com>
2025-12-14 15:09:49 +0700

    feat: click on logo redirect to main page

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -4,7 +4,7 @@ import {
   Logo,
   ProfileIcon,
 } from '@krgaa/react-developer-burger-ui-components';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import styles from './app-header.module.css';
 
@@ -58,7 +58,9 @@ export const AppHeader = (): React.JSX.Element => {
           ))}
         </div>
         <div className={styles.logo}>
-          <Logo />
+          <Link to="/">
+            <Logo />
+          </Link>
         </div>
         <div className={styles.menu_part_right}>
           {rightLinks.map((link) => (
